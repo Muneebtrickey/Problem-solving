8a1fc318521a4f9911c57e0ada5edf7369f40c83
Muneebtrickey <itsmuneeb566@gmail.com>
2024-07-24 15:20:59 +0500

    problem 3 24/07/2024

--- constructing_list_using_list_comprehension.py
+++ constructing_list_using_list_comprehension.py
@@ -3,5 +3,18 @@
 #by using list comprehension we can construct the list like
 # this
 
-List = [i * (i-1) for i in range(1,11)]
-print(List)
+
+L = [i * (i-1) for i in range(1,11)]
+print(L)
+
+
+
+
+
+# another approach
+
+List = [(i * (i + 1))//2 for i in range(10) ]
+
+
+another_list = [2 * i for i in List]
+print(another_list)
