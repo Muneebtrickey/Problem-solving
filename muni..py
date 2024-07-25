8679271e0ef11eb7111ed8ca6e23fc48cb9e8378
Muneebtrickey <itsmuneeb566@gmail.com>
2024-07-25 12:34:27 +0500

    25/07/2024 reads inputs

--- muni..py
+++ muni..py
@@ -1,3 +1,11 @@
+import sys 
+print("Enter your line press ctrl + z if you want to end.")
+try:
+    lines = sys.stdin.readlines()
 
-list = [i * (i-1) for i in range(1,11)]
-print(list)
+except EOFError:
+    print("End of the file. ")
+
+finally:
+    for item in reversed(lines):
+        print(item.strip())
