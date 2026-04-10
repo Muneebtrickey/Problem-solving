8f967ce574da4f7d83afe943f57bb71576e2afaf
Muneebtrickey <itsmuneeb566@gmail.com>
2026-04-10 22:59:44 +0500

    check equal occurance of all characters

---
+++ April_coding_practice_2026/check_equal_occurances.py
@@ -0,0 +1,25 @@
+"""
+Example 3: 1941. Check if All Characters Have Equal Number of Occurrences
+
+Given a string s, determine if all characters have the same frequency.
+
+For example, given s = "abacbc", return true, because all characters appear twice.
+ Given s = "aaabb", return false. "a" appears 3 times, "b" appears 2 times. 3 != 2.
+"""
+
+
+def check_occurance(s):
+    dic = {}
+    check_set = set()
+
+    for char in s:
+        dic[char] = dic.get(char, 0) + 1
+    
+
+    for value in dic.values():
+        check_set.add(value)
+
+    return len(check_set) == 1
+
+
+print(check_occurance('abacbc'))
